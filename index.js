269f9e52f81d83e372433d8f253411d398a6f182
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-06-09 08:58:17 +0700

    feat: fetch data & sorting

---
+++ index.js
@@ -0,0 +1,46 @@
+const container = document.querySelector(".grid-container");
+const search = document.getElementById("search");
+let list = [];
+
+const render = (data) => {
+  container.innerHTML = "";
+  data.forEach((item) => {
+    const card = document.createElement("div");
+    const content = document.createElement("div");
+    const name = document.createElement("span");
+    const img = document.createElement("img");
+
+    card.classList.add("card");
+    img.classList.add("card-image");
+    img.src = item.image;
+    content.classList.add("card-content");
+    name.textContent = item.name;
+
+    content.appendChild(name);
+    card.appendChild(img);
+    card.appendChild(content);
+
+    container.appendChild(card);
+  });
+};
+
+const fetchData = async () => {
+  try {
+    const res = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await res.json();
+    list = data.results;
+    render(list);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+search.addEventListener("input", (e) => {
+  const k = e.target.value.toLowerCase();
+  const f = list.filter((v) =>
+    v.name.toLowerCase().includes(k)
+  );
+  render(f);
+});
+
+fetchData();
